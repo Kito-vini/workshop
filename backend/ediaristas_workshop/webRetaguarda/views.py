7cf6ca6daf818ea7fe1c8a0c7c827486c8faf0ac
kito-vini <marcusviva11@gmail.com.br>
2021-07-08 19:38:16 -0300

    Criação da função de cadastro

--- backend/ediaristas_workshop/webRetaguarda/views.py
+++ backend/ediaristas_workshop/webRetaguarda/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import diarista_form
+from .models import Diaristas
 
 # Create your views here.
 
+#Funcção para cadastrar as diaristas
 def cadastrar_diarista(request):
-    form_diarista = diarista_form.DiaristaForm()
+    if request.method =="POST":
+        form_diarista = diarista_form.DiaristaForm(request.POST, request.FILES)
+        if form_diarista.is_valid():
+            form_diarista.save()
+            return redirect('listar_diaristas')
+    else:
+        form_diarista = diarista_form.DiaristaForm()
     return render(request, 'form_diarista.html', {'form_diarista' : form_diarista})
+
+#Função para listar as diaristas do banco de dados
+def listar_diaristas(request):
+    diaristas = Diaristas.objects.all()
+    return render(request, 'lista_diaristas.html', {'diaristas': diaristas})
