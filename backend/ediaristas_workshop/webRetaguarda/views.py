eb2a3f1a2b4b26c13d44f1ee95316ffcef5e7435
kito-vini <marcusviva11@gmail.com.br>
2021-07-09 21:07:19 -0300

    criação da função editar diaristas

--- backend/ediaristas_workshop/webRetaguarda/views.py
+++ backend/ediaristas_workshop/webRetaguarda/views.py
@@ -4,7 +4,7 @@ from .models import Diaristas
 
 # Create your views here.
 
-#Funcção para cadastrar as diaristas
+#Função para cadastrar as diaristas
 def cadastrar_diarista(request):
     if request.method =="POST":
         form_diarista = diarista_form.DiaristaForm(request.POST, request.FILES)
@@ -19,3 +19,12 @@ def cadastrar_diarista(request):
 def listar_diaristas(request):
     diaristas = Diaristas.objects.all()
     return render(request, 'lista_diaristas.html', {'diaristas': diaristas})
+
+#Função para editar as diaristas
+def editar_diarista(request, diaristas_id):
+    diarista = Diaristas.object.get(id=diaristas_id)
+    form_diarista = diarista_form.DiaristaForm(request.POST or None, instance=diarista)
+    if form_diarista.is_valid():
+        form_diarista.save()
+        return redirect('listar_diaristas')
+    return render(request, 'form_diarista.html', {'form_diarista': form_diarista})
